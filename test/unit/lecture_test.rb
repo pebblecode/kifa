999242ef6f0a5ff1c42c4c31d91bccf815bfb185
George Ornbo <george@shapeshed.com>
2013-03-05 12:11:16 +0000

    test length of title

--- test/unit/lecture_test.rb
+++ test/unit/lecture_test.rb
@@ -5,8 +5,20 @@ describe Lecture do
     @lecture = Lecture.new
   end
 
-  it "should be invalid if a name and link are not provided" do
+  it "should be invalid if the title is not provided" do
+    @lecture.title = false
     @lecture.valid?.wont_equal true
   end
+
+  it "should be invalid if the title is longer than 200 characters" do
+    @lecture.title =  Random.alphanumeric(size = 201)
+    @lecture.valid?.wont_equal true
+  end
+
+  it "should be valid if the title is present and less than 200 characters" do
+    @lecture.title =  Random.alphanumeric(size = 199)
+    @lecture.valid?.must_equal true
+  end
+
 end
 

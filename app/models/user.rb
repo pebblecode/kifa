5281f42b76b945eaaa8dd2c704fd42818ac83529
George Ornbo <george@shapeshed.com>
2013-03-05 15:23:15 +0000

    add omniauth stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,25 @@
 class User < ActiveRecord::Base
-  attr_accessible :username
+
+  devise :omniauthable, :omniauth_providers => [:twitter]
+
+  attr_accessible :username, :provider, :uid
 
   has_many :lectures
+
   validates :username,
     :presence => true,
     :length => {
       :maximum => 200
     }
+
+
+    def self.find_for_twitter_oauth(access_token, signed_in_resource=nil)
+      data = access_token.extra.raw_info
+      if user = User.where(:username => data.screen_name).first
+        user
+      else
+        User.create!(:username => data.screen_name)
+      end
+    end
 
 end

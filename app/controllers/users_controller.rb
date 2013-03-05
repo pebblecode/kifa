9f47ea2b135c8918fed5954b9037b50a689fe9bd
Vince Martínez <vincent@pebblecode.com>
2013-03-05 13:15:33 +0000

    Users controller added

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  respond_to :json
+  before_filter :find_user, :only => [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+    respond_with @users
+  end
+
+  def show
+    respond_with @user
+  end
+
+  def create
+    @user = User.new(params[:user])
+    respond_with @user
+  end
+
+  def update
+    @user.update_attributes(params[:user])
+    respond_with @user
+  end
+
+  def destroy
+    @user.destroy
+    respond_with @user
+  end
+
+  private
+    def find_user
+      @user = User.find(params[:id])
+    end
+
+end

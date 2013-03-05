955efd5352566614e9bb4e7a75988cac2c9ebbaf
George Ornbo <george@shapeshed.com>
2013-03-05 10:03:05 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kifa::Application.config.secret_token = '0b521727889636ab38d39f7700ce555fb6202d11e0ca59856023f57edf0add69b7335934d71ebe7314f3f2f98f195e6bff7feb347f24ab3497e9ce9e2fe0a963'
